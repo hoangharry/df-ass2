36ce412a3d1a2f7547f9e447d691cf37c67c8e6c
Hoang Ho Minh <hmhoang@Hoangs-MacBook-Pro.local>
2022-04-20 17:19:33 +0700

    [update] add migration and makefile for db

---
+++ db/migration/000001_init_schema.up.sql
@@ -0,0 +1,15 @@
+-- CREATE TABLE "User" (
+--     "id" BIGINT AUTO_INCREMENT PRIMARY KEY,
+--     "user_name" VARCHAR NOT NULL,
+--     "pwd" TEXT NOT NULL
+-- );
+
+CREATE TABLE "Article" (
+    "id" BIGSERIAL PRIMARY KEY,
+    "category" VARCHAR NOT NULL,
+    "user_id" BIGINT,
+    "description" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT (now())
+);
+
