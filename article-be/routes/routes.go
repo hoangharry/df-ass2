cc22d96e5bde1a4a6d892d80e825f6f9ff35bb7a
hoangharry <hoang.ho.it99@gmail.com>
2022-04-29 09:22:47 +0700

    [update] fix typo

--- article-be/routes/routes.go
+++ article-be/routes/routes.go
@@ -8,7 +8,7 @@ import (
 func Setup(router *gin.Engine) {
 	v1 := router.Group("/v1")
 	{
-		v1.POST("/aticle", controllers.PostArticle)
+		v1.POST("/article", controllers.PostArticle)
 		v1.GET("/article/:id", controllers.GetArticleById)
 		v1.GET("/article", controllers.GetArticles)
 	}
