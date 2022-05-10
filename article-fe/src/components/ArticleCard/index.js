07a2a0a9ffa078dba94422f9d4214608e27b60fd
hoangharry <hoang.ho.it99@gmail.com>
2022-05-10 10:25:52 +0700

    feat(fe): update new article card

--- article-fe/src/components/ArticleCard/index.js
+++ article-fe/src/components/ArticleCard/index.js
@@ -1,11 +1,31 @@
+import { CalendarIcon, UserIcon } from "@heroicons/react/outline";
+
 const ArticleCard = ({ article }) => {
     return (
-        <div className="bg-white dark:bg-slate-900 hover:bg-violet-600 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
-        <h3 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">{article.title}</h3>
-        <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm">
-            {article.description}
-        </p>
+        <div class="rounded overflow-hidden shadow-lg hover:bg-violet-100">
+        <img class="w-full h-80" src="https://www.w3schools.com/images/lamp.jpg" alt="Mountain" />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2 hover:text-violet-700">Mountain</div>
+          <p class="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          </p>
         </div>
+        <div class="px-6 items-center">
+            <div className="flex inline-block">
+            <UserIcon className="w-4 h-4"/>
+            <p class="text-gray-900 leading-none">John Smith</p>
+            </div>
+          <div class="text-sm flex inline-block mt-2">
+            <CalendarIcon className="w-4 h-4"/>
+            <p class="text-gray-600">Aug 18</p>
+          </div>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+        </div>
+      </div>
     );
 }
 
