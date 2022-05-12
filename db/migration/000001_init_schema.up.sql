c13b586424164294270f6400fd05cbc225c7e0ea
hoangharry <hoang.ho.it99@gmail.com>
2022-05-12 15:04:50 +0700

    feat(db): update db migration schema

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -7,14 +7,17 @@
 CREATE TABLE "article" (
     "id" BIGSERIAL PRIMARY KEY,
     "category" VARCHAR NOT NULL,
-    "user_id" BIGINT,
+    "user_id" INT,
+    "img" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "content" TEXT NOT NULL,
-    "created_at" BIGINT NOT NULL
+    "created_at" TIMESTAMP WITH TIMEZONE NOT NULL,
+    "is_deleted" INT,
 );
 CREATE Table "user" (
     "id" BIGSERIAL PRIMARY KEY,
-    "name" VARCHAR NOT NULL
+    "username" VARCHAR NOT NULL,
+    "pwd" VARCHAR NOT NULL,
 );
 
